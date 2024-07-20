a5286459ebb45b9668d797672782cc8151a8b18a
maninisheth <maninisheth@gmail.com>
2024-07-20 15:29:39 +0100

    change name and surname in two different line

--- src/main/java/com/studentapp/printMyName.java
+++ src/main/java/com/studentapp/printMyName.java
@@ -3,6 +3,8 @@ package com.studentapp;
 public class printMyName {
 
     public static void main(String[] args) {
-        System.out.println("Manini Sheth");
+        System.out.println("Manini");
+        System.out.println("sheth");
+
     }
 }
